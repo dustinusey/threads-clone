f03b546b7f4b86b4341b8e5386361f6a0a9c353d
dustin <dustinusey@me.com>
2024-05-02 13:56:27 -0500

    added user image from google cred

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -1,6 +1,10 @@
 import PostBtn from "./PostBtn";
 
+import { useContext } from "react";
+import { UserContext } from "./UserProvider";
+
 const CreatePost = (props) => {
+  const { user } = useContext(UserContext);
   const { setCreatePost, setOverlay } = props;
   return (
     <div
@@ -10,7 +14,9 @@ const CreatePost = (props) => {
       }}
       className="w-full max-w-[615px] m-auto mt-5 text-[#B3B3B3] dark:text-[#767D83] flex items-center pb-5 px-3"
     >
-      <div className="rounded-full bg-zinc-300 dark:bg-zinc-700 size-[35px] mr-3"></div>
+      <div className="rounded-full bg-zinc-300 dark:bg-zinc-700 size-[35px] mr-3">
+        {user && <img src={`${user.photoURL}.jpg`} />}
+      </div>
       <p>Start a thread...</p>
       <PostBtn />
     </div>
